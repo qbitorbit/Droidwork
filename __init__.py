32ca1d349b6046f6e414e98156a8fbb534b37216
qbitorbit <qbitorbit@gmail.com>
2025-12-05 21:37:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,34 @@
-"""
-Android Tools Module
-Provides LangChain tools for Android device control via ADB.
-"""
+# """
+# Android Tools Module
+# Provides LangChain tools for Android device control via ADB.
+# """
+
+# from .adb_client import ADBClient
+# from .device_manager import (
+#     DeviceManager,
+#     list_android_devices,
+#     get_device_info,
+#     get_device_battery_info,
+#     reboot_device,
+#     get_device_screen_info,
+# )
+
+# __all__ = [
+#     # Base client
+#     'ADBClient',
+    
+#     # Device manager
+#     'DeviceManager',
+    
+#     # Device management tools
+#     'list_android_devices',
+#     'get_device_info',
+#     'get_device_battery_info',
+#     'reboot_device',
+#     'get_device_screen_info',
+# ]
+
+"""Android Tools Module"""
 
 from .adb_client import ADBClient
 from .device_manager import (
@@ -12,18 +39,32 @@ from .device_manager import (
     reboot_device,
     get_device_screen_info,
 )
+from .app_control import (
+    AppController,
+    list_installed_packages,
+    get_app_info,
+    # install_apk,  # Not implemented yet
+    # uninstall_app,  # Not implemented yet
+    # start_app,  # Not implemented yet
+    # stop_app,  # Not implemented yet
+    # clear_app_data,  # Not implemented yet
+)
 
 __all__ = [
-    # Base client
     'ADBClient',
-    
-    # Device manager
     'DeviceManager',
-    
-    # Device management tools
     'list_android_devices',
     'get_device_info',
     'get_device_battery_info',
     'reboot_device',
     'get_device_screen_info',
+    'AppController',
+    'list_installed_packages',
+    'get_app_info',
+    # Uncomment as we implement them:
+    # 'install_apk',
+    # 'uninstall_app',
+    # 'start_app',
+    # 'stop_app',
+    # 'clear_app_data',
 ]
